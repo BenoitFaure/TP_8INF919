c0ae846ab0ff6743ca2f4c6346dfe76352a4a923
BenoitFaure <benoit.faure.cesar@gmail.com>
2023-11-30 19:45:49 -0500

    Checkpoint - dev_2_1 0.8515 - res9

--- devoir_2.py
+++ devoir_2.py
@@ -1,19 +1,74 @@
+# Classification on CIFAR-10 dataset using a not pretrained ResNet-18
+
+# Dependencies
+import datetime
 import numpy as np
 
 import tensorflow as tf
-from tensorflow.keras.datasets import cifar10
-from tensorflow.keras import Model
-from tensorflow.keras.layers import Input, Conv2D, Flatten, Dense, Dropout, AveragePooling2D, BatchNormalization, Activation, Add, MaxPooling2D
-from tensorflow.keras.losses import CategoricalCrossentropy
+
+# ------------------- Configure GPU ------------------- #
+num_partitions = 4
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+print("Num GPUs Available: ", len(gpus))
+
+if not len(gpus):
+    exit()
+
+# Get memory limit on gpu
+memory_limit = 2048 # MB
+mem_limit_per_partition = int(memory_limit / num_partitions)
+
+mem_limit_per_partition = 512 # MB
+
+# Set memory limit on gpu
+tf.config.experimental.set_virtual_device_configuration(gpus[0], [
+    tf.config.experimental.VirtualDeviceConfiguration(memory_limit=mem_limit_per_partition) 
+    for _ in range(num_partitions)
+])
+
+# ------------------- tf imports -------------------
+
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.layers import Input, Conv2D, Flatten, Dense, Dropout, \
+    AveragePooling2D, BatchNormalization, Activation, Add, MaxPooling2D
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers.experimental.preprocessing import RandomRotation, \
+      RandomZoom, RandomFlip, RandomTranslation, Resizing
+from tensorflow.keras.losses import CategoricalCrossentropy
+from tensorflow.keras.callbacks import ModelCheckpoint
 
-import matplotlib.pyplot as plt
+# ------------------- Settings ------------------- #
 
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+# Data settings
+resize = (224, 224)
 
-# -------------- Load CIFAR10 dataset --------------
+# Model settings
+initializer = tf.initializers.he_normal(seed=42)
+classification_initializer = tf.initializers.glorot_normal(seed=42)
 
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+activation_function = 'relu' # 'selu'
+use_bias = False # Batch Norm takes care of it
+epsilon_bn = 1.001e-5
+kernel_reg = None # tf.keras.regularizers.l2(0.0001)
+
+# Training settings
+epochs = 90
+batch_size = 8 # 64
+
+# Define optimizer
+learning_rate = 0.005
+# optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+optimizer = tf.keras.optimizers.Adam(
+    learning_rate=learning_rate) #, clipvalue=0.1)
+# optimizer = tf.keras.optimizers.RMSprop(learning_rate=0.01)
+
+# Define loss
+loss = CategoricalCrossentropy(label_smoothing=0.2)
+
+# ------------------- Load data ------------------- #
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
 # Print dimensions of training data
 print("Training data shape: ", x_train.shape)
@@ -27,157 +82,147 @@ print("Test labels shape: ", y_test.shape)
 num_classes = len(np.unique(y_train))
 print("Number of classes: ", num_classes)
 
-# -------------- Preprocess data --------------
-# Normalize images
-x_train = x_train.astype('float32') / 255
-x_test = x_test.astype('float32') / 255
-
-# # Data augmentation
-# def data_augment(x):
-#     x = RandomRotation(0.2)(x)
-#     x = RandomTranslation(0.2, 0.2)(x)
-#     return x
-
-# x_train = data_augment(x_train)
+# ------------------- Preprocess data ------------------- #
 
 # One hot encode labels
 y_train = to_categorical(y_train, num_classes)
 y_test = to_categorical(y_test, num_classes)
 
+# Load as tf dataset
+train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
 
-# -------------- Declare Layers --------------
+# Normalize images
+def normalize_img(image, label):
 
-initializer = tf.keras.initializers.RandomUniform(minval=-0.05, maxval=0.05, seed=42)
+    # Resize images
+    image = tf.image.resize(image, resize)
+    image = tf.cast(image, tf.float32) / 255.
 
+    return image, label
+
+# Normalise images
+ds_train = train_dataset.map(normalize_img)
+ds_test = test_dataset.map(normalize_img)
+
+# ------------------- Define model ------------------- #
+
+# Identity block
 def identity_block(filter, kernel_size=3):
     def _identity_block(x):
         input_x = x
+
         # Layer 1
-        x = Conv2D(filter, kernel_size=kernel_size, padding = 'same', kernel_initializer=initializer)(x)
-        x = BatchNormalization(axis=3)(x)
-        x = Activation('selu')(x)
+        x = Conv2D(filter, kernel_size=kernel_size, 
+                   padding = 'same', kernel_initializer=initializer,
+                   use_bias=use_bias, kernel_regularizer=kernel_reg)(x)
+        x = BatchNormalization(axis=3, epsilon=epsilon_bn)(x)
+        x = Activation(activation_function)(x)
+
         # Layer 2
-        x = Conv2D(filter, kernel_size=kernel_size, padding = 'same', kernel_initializer=initializer)(x)
-        x = BatchNormalization(axis=3)(x)
-        x = Activation('selu')(x)
+        x = Conv2D(filter, kernel_size=kernel_size, 
+                   padding = 'same', kernel_initializer=initializer,
+                   use_bias=use_bias, kernel_regularizer=kernel_reg)(x)
+        x = BatchNormalization(axis=3, epsilon=epsilon_bn)(x)
+
         # Add Residue
         x = Add()([x, input_x])
+        
+        # Activation
+        x = Activation(activation_function)(x)
+
         return x
     
     return _identity_block
 
-def conv_res_block(filter, kernel_size=3):
-    def _conv_res_block(x):
+# Convolutional Res block
+def convolutional_res_block(filter, kernel_size=3):
+    def _convolutional_res_block(x):
         input_x = x
 
         # Layer 1
-        x = Conv2D(filter, kernel_size=kernel_size, padding = 'same', kernel_initializer=initializer)(x)
-        x = BatchNormalization(axis=3)(x)
-        x = Activation('selu')(x)
-        # Layer 2
-        x = Conv2D(filter, kernel_size=kernel_size, padding = 'same', kernel_initializer=initializer)(x)
-        x = BatchNormalization(axis=3)(x)
-        x = Activation('selu')(x)
+        x = Conv2D(filter, kernel_size=kernel_size, 
+                   padding = 'same', kernel_initializer=initializer,
+                   use_bias=use_bias, kernel_regularizer=kernel_reg)(x)
+        x = BatchNormalization(axis=3, epsilon=epsilon_bn)(x)
+        x = Activation(activation_function)(x)
 
-        # Residue Layer
-        input_x = Conv2D(filter, kernel_size=1, padding = 'same', kernel_initializer=initializer)(input_x)
+        # Layer 2
+        x = Conv2D(filter, kernel_size=kernel_size, 
+                   padding = 'same', kernel_initializer=initializer,
+                   use_bias=use_bias, kernel_regularizer=kernel_reg)(x)
+        x = BatchNormalization(axis=3, epsilon=epsilon_bn)(x)
+
+        # Layer 3
+        input_x = Conv2D(filter, kernel_size=kernel_size, 
+                   padding = 'same', kernel_initializer=initializer,
+                   use_bias=use_bias, kernel_regularizer=kernel_reg)(input_x)
+        input_x = BatchNormalization(axis=3, epsilon=epsilon_bn)(input_x)
 
         # Add Residue
         x = Add()([x, input_x])
+        
+        # Activation
+        x = Activation(activation_function)(x)
+
         return x
     
-    return _conv_res_block
+    return _convolutional_res_block
 
-# -------------- Declare the model --------------
+# Convolutional block
+def convolutional_block(filter, kernel_size=3, strides=2):
+    def _convolutional_block(x):
+        
+        x = Conv2D(filter, kernel_size=kernel_size, 
+                   padding = 'same', kernel_initializer=initializer,
+                   use_bias=use_bias, strides=strides, kernel_regularizer=kernel_reg)(x)
+        x = BatchNormalization(axis=3, epsilon=epsilon_bn)(x)
+        x = Activation(activation_function)(x)
 
-input_shape = x_train.shape[1:]
+        return x
+    
+    return _convolutional_block
+
+# Define model
+
+input_shape = (resize[0], resize[1], 3)
 input_layer = Input(shape=input_shape)
 
-# Feature Extractor
-def conv_block(filter, kernel_size = 3, stride = 2):
-    def _conv_block(x):
-        x = Conv2D(filter, kernel_size=kernel_size, strides=stride, padding='same', kernel_initializer=initializer)(x)
-        x = BatchNormalization()(x)
-        x = Activation('selu')(x)
-        return x
-    return _conv_block
-
-def conv_pool_block(filter, kernel_size = 3):
-    def _conv_pool_block(x):
-        x = Conv2D(filter, kernel_size=kernel_size, strides=2, padding='same', kernel_initializer=initializer)(x)
-        x = BatchNormalization()(x)
-        x = MaxPooling2D(pool_size=(2, 2), padding = 'same')(x)
-        x = Activation('selu')(x)
-        return x
-    return _conv_pool_block
-
-# conv_net = [
-#     conv_pool_block(64),
-#     identity_block(64),
-#     conv_pool_block(128),
-#     identity_block(128),
-#     conv_pool_block(256),
-#     identity_block(256),
-#     AveragePooling2D(pool_size=(4, 4), padding = 'same'),
-# ]
-
-# conv_net = [
-#     conv_pool_block(64),
-#     identity_block(64),
-#     conv_pool_block(128),
-#     identity_block(128),
-#     AveragePooling2D(pool_size=(2, 2), padding = 'same'),
-# ]
-
-# conv_net = [
-#     conv_pool_block(64),
-#     conv_res_block(64),
-#     conv_pool_block(128),
-#     conv_res_block(128),
-#     AveragePooling2D(pool_size=(2, 2), padding = 'same'),
-# ]
-
-# conv_net = [
-#     conv_block(64, kernel_size = 7, stride = 1),
-#     conv_res_block(64),
-#     conv_pool_block(128, kernel_size = 7),
-#     conv_res_block(128),
-#     conv_res_block(128),
-#     conv_block(256, kernel_size = 3, stride = 2),
-#     conv_pool_block(256, kernel_size = 3),
-# ] # 0.79
-
-conv_net = [
-    conv_block(64, kernel_size = 7, stride = 1),
-    conv_res_block(64),
-    conv_res_block(64),
-    conv_pool_block(128, kernel_size = 5),
-    conv_res_block(128),
-    conv_res_block(128),
-    conv_block(256, kernel_size = 3, stride = 2),
-    conv_pool_block(256, kernel_size = 3),
+data_aug = [
+    RandomRotation(0.1, seed=42),
+    RandomTranslation(0.1, 0.1, seed=42),
+    RandomZoom(0.1, seed=42),
+    RandomFlip(seed=42),
 ]
 
-# conv_net = [
-#     Conv2D(32, kernel_size=(3, 3), strides=1, activation='relu', padding='same'),
-#     Conv2D(64, kernel_size=(3, 3), strides=2, activation='relu', padding='same'),
-#     Conv2D(128, kernel_size=(3, 3), strides=2, activation='relu', padding='same'),
-#     Conv2D(256, kernel_size=(3, 3), strides=2, activation='relu', padding='same'),
-#     AveragePooling2D(pool_size=(4, 4), padding = 'same'),
-# ]
+model_name = 'ResNet-18'
+conv_net = [
+    convolutional_block(64, kernel_size = 7, strides = 2),
+    AveragePooling2D(pool_size=3, strides = 2, padding = 'same'),
 
-# Classifier
+    identity_block(64),
+    identity_block(64),
+
+    convolutional_block(128),
+    identity_block(128),
+
+    convolutional_block(256),
+    identity_block(256),
+
+    convolutional_block(512),
+    identity_block(512),
+
+    AveragePooling2D(pool_size=7)
+]
+
 classification_net = [
     Flatten(),
-    Dense(256, activation='selu', kernel_initializer=initializer),
+    Dense(512, kernel_initializer=initializer, activation=activation_function),
     Dropout(0.5),
-    Dense(num_classes, activation='softmax', kernel_initializer=initializer)
+    Dense(num_classes, kernel_initializer=classification_initializer, activation='softmax')
 ]
 
-# Train settings
-epochs = 30
-batch_size = 64
-
+# Compile function
 def create_classifier():
 
     def compile_layers(input, layers):
@@ -185,49 +230,83 @@ def create_classifier():
             input = layer(input)
         return input
 
+    # Build Data augmentation
+    data_augmentation_layers = compile_layers(input_layer, data_aug)
+
     # Build Feature Extractor
-    conv_net_layers = compile_layers(input_layer, conv_net)
+    conv_net_layers = compile_layers(data_augmentation_layers, conv_net)
 
     # Build Classifier
     classification_layers = compile_layers(conv_net_layers, classification_net)
 
+    output_layer = classification_layers
+
     # Build model
-    model = Model(inputs=input_layer, outputs=classification_layers)
+    model = Model(inputs=input_layer, outputs=output_layer)
 
     return model
 
-def train_test_model(model):
+# ------------------- Prepare Data ------------------- #
 
-    # Define optimizer
-    optimizer = 'adam'
+strategy = tf.distribute.MirroredStrategy()
+
+# Configure Dataset for Performance
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+# ds_train = ds_train.cache()
+ds_train = ds_train.batch(batch_size)
+ds_train = ds_train.prefetch(buffer_size=AUTOTUNE)
+
+ds_test = ds_test.batch(batch_size)
+ds_test = ds_test.prefetch(buffer_size=AUTOTUNE)
+
+del x_train, y_train, x_test, y_test
+
+# ------------------- Prepare model ------------------- #
+
+with strategy.scope():
+    # Create model
+    model = create_classifier()
 
     # Compile model
-    model.compile(optimizer=optimizer, loss=CategoricalCrossentropy(label_smoothing=0.2), metrics=['accuracy'])
+    model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
+
+# Summary
+model.summary()
+
+# ------------------- Train model ------------------- #
+
+# Define train function
+def train_test_model(model):
+
+    # Callbacks
+    name_str = datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + '_' + model_name + '_' + str(num_partitions)
+
+    #   Save model callback
+    model_loc = 'models/' + name_str + '.h5'
+    checkpoint = ModelCheckpoint(model_loc, monitor='val_accuracy', 
+                                save_best_only=True, mode='max', verbose=1)
+    
+    print('Model saved at: ' + model_loc)
+
+    #   Tensorboard callback
+    log_dir = "logs/fit/" + name_str
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="logs/fit/" + name_str, histogram_freq=1)
+
+    print('tensorboard --logdir ' + log_dir)
 
     # Train model
-    run_hist = model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, 
-              validation_data=(x_test, y_test))
+    run_hist = model.fit(ds_train, validation_data=ds_test,
+                         epochs=epochs, batch_size=batch_size,
+                         callbacks=[checkpoint, tensorboard_callback])
     
     return run_hist
-
-# -------------- Train and test the model --------------
-
-# Create model
-model = create_classifier()
-
-model.summary()
-# exit()
 
 # Run train and validation
 run_hist = train_test_model(model)
 
-# Run test
-test_loss, test_acc = model.evaluate(x_test,  y_test, verbose=2)
-print(f'Test accuracy: {test_acc}')
+# ------------------- Evaluate model ------------------- #
 
-# Plot accuracy and loss
-plt.plot(run_hist.history['accuracy'], label='train')
-plt.plot(run_hist.history['val_accuracy'], label='test')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.show()
+# Evaluate model
+test_loss, test_acc = model.evaluate(ds_test, verbose=2)
+print(f'Test accuracy: {test_acc}')
--- devoir_2_1.py
+++ devoir_2_1.py
@@ -1,14 +1,53 @@
+# Classification on CIFAR-10 dataset using a not pretrained ResNet-18
+
+# Dependencies
+import datetime
 import numpy as np
 
 import tensorflow as tf
-from tensorflow.keras.datasets import cifar10
-from tensorflow.keras import layers, Sequential
 
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+# ------------------- tf imports -------------------
 
-# -------------- Load CIFAR10 dataset --------------
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.layers import Input, Conv2D, Flatten, Dense, Dropout, \
+    AveragePooling2D, BatchNormalization, Activation, Add, MaxPooling2D
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers.experimental.preprocessing import RandomRotation, \
+      RandomZoom, RandomFlip, RandomTranslation, Resizing
+from tensorflow.keras.losses import CategoricalCrossentropy
+from tensorflow.keras.callbacks import ModelCheckpoint
 
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+# ------------------- Settings ------------------- #
+
+# Data settings
+resize = (32, 32)
+
+# Model settings
+initializer = tf.initializers.he_normal(seed=42)
+classification_initializer = tf.initializers.glorot_normal(seed=42)
+
+activation_function = 'relu' # 'selu'
+use_bias = False # Batch Norm takes care of it
+epsilon_bn = 1.001e-5
+kernel_reg = None # tf.keras.regularizers.l2(0.0001)
+
+# Training settings
+epochs = 1000 # 90
+batch_size = 256 # 64
+
+# Define optimizer
+learning_rate = 0.005
+# optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+optimizer = tf.keras.optimizers.Adam(
+    learning_rate=learning_rate, clipvalue=0.12)
+# optimizer = tf.keras.optimizers.RMSprop(learning_rate=0.01)
+
+# Define loss
+loss = CategoricalCrossentropy(label_smoothing=0.2)
+
+# ------------------- Load data ------------------- #
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
 # Print dimensions of training data
 print("Training data shape: ", x_train.shape)
@@ -22,20 +61,229 @@ print("Test labels shape: ", y_test.shape)
 num_classes = len(np.unique(y_train))
 print("Number of classes: ", num_classes)
 
-# -------------- Preprocess data --------------
+# ------------------- Preprocess data ------------------- #
 
-# Preprocess block
+# One hot encode labels
+y_train = to_categorical(y_train, num_classes)
+y_test = to_categorical(y_test, num_classes)
 
-data_augmentation = Sequential(
-    [
-        layers.Normalization(),
-        # layers.Resizing(image_size, image_size),
-        layers.RandomFlip("horizontal"),
-        layers.RandomRotation(factor=0.02),
-        layers.RandomZoom(
-            height_factor=0.2, width_factor=0.2
-        ),
-    ],
-    name="data_augmentation",
-)
+# Load as tf dataset
+train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
 
+# Normalize images
+def normalize_img(image, label):
+
+    # Resize images
+    image = tf.image.resize(image, resize)
+    image = tf.cast(image, tf.float32) / 255.
+
+    return image, label
+
+# Normalise images
+ds_train = train_dataset.map(normalize_img)
+ds_test = test_dataset.map(normalize_img)
+
+# ------------------- Define model ------------------- #
+
+# Identity block
+def identity_block(filter, kernel_size=3):
+    def _identity_block(x):
+        input_x = x
+
+        # Layer 1
+        x = Conv2D(filter, kernel_size=kernel_size, 
+                   padding = 'same', kernel_initializer=initializer,
+                   use_bias=use_bias, kernel_regularizer=kernel_reg)(x)
+        x = BatchNormalization(axis=3, epsilon=epsilon_bn)(x)
+        x = Activation(activation_function)(x)
+
+        # Layer 2
+        x = Conv2D(filter, kernel_size=kernel_size, 
+                   padding = 'same', kernel_initializer=initializer,
+                   use_bias=use_bias, kernel_regularizer=kernel_reg)(x)
+        x = BatchNormalization(axis=3, epsilon=epsilon_bn)(x)
+
+        # Add Residue
+        x = Add()([x, input_x])
+        
+        # Activation
+        x = Activation(activation_function)(x)
+
+        return x
+    
+    return _identity_block
+
+# Convolutional Res block
+def convolutional_res_block(filter, kernel_size=3):
+    def _convolutional_res_block(x):
+        input_x = x
+
+        # Layer 1
+        x = Conv2D(filter, kernel_size=kernel_size, 
+                   padding = 'same', kernel_initializer=initializer,
+                   use_bias=use_bias, kernel_regularizer=kernel_reg)(x)
+        x = BatchNormalization(axis=3, epsilon=epsilon_bn)(x)
+        x = Activation(activation_function)(x)
+
+        # Layer 2
+        x = Conv2D(filter, kernel_size=kernel_size, 
+                   padding = 'same', kernel_initializer=initializer,
+                   use_bias=use_bias, kernel_regularizer=kernel_reg)(x)
+        x = BatchNormalization(axis=3, epsilon=epsilon_bn)(x)
+
+        # Layer 3
+        input_x = Conv2D(filter, kernel_size=kernel_size, 
+                   padding = 'same', kernel_initializer=initializer,
+                   use_bias=use_bias, kernel_regularizer=kernel_reg)(input_x)
+        input_x = BatchNormalization(axis=3, epsilon=epsilon_bn)(input_x)
+
+        # Add Residue
+        x = Add()([x, input_x])
+        
+        # Activation
+        x = Activation(activation_function)(x)
+
+        return x
+    
+    return _convolutional_res_block
+
+# Convolutional block
+def convolutional_block(filter, kernel_size=3, strides=2):
+    def _convolutional_block(x):
+        
+        x = Conv2D(filter, kernel_size=kernel_size, 
+                   padding = 'same', kernel_initializer=initializer,
+               
+                   use_bias=use_bias, strides=strides, kernel_regularizer=kernel_reg)(x)
+        x = BatchNormalization(axis=3, epsilon=epsilon_bn)(x)
+        x = Activation(activation_function)(x)
+
+        return x
+    
+    return _convolutional_block
+
+# Define model
+
+input_shape = (resize[0], resize[1], 3)
+input_layer = Input(shape=input_shape)
+
+data_aug = [
+    RandomRotation(0.1, seed=42),
+    RandomTranslation(0.1, 0.1, seed=42),
+    RandomZoom(0.1, seed=42),
+    RandomFlip(seed=42),
+]
+
+model_name = 'ResNet-9'
+conv_net = [
+
+    convolutional_block(64, strides=1),
+    convolutional_block(128, strides=1),
+    MaxPooling2D(pool_size=2, strides=2),
+
+    identity_block(128, kernel_size=3),
+
+    convolutional_block(256, strides=1),
+    convolutional_block(512, strides=1),
+    MaxPooling2D(pool_size=2, strides=2),
+
+    identity_block(512, kernel_size=3),
+
+    MaxPooling2D(pool_size=8, strides=1)
+]
+
+classification_net = [
+    Flatten(),
+    Dense(512, kernel_initializer=initializer, activation=activation_function),
+    Dropout(0.5),
+    Dense(num_classes, kernel_initializer=classification_initializer, activation='softmax')
+]
+
+# Compile function
+def create_classifier():
+
+    def compile_layers(input, layers):
+        for layer in layers:
+            input = layer(input)
+        return input
+
+    # Build Data augmentation
+    data_augmentation_layers = compile_layers(input_layer, data_aug)
+
+    # Build Feature Extractor
+    conv_net_layers = compile_layers(data_augmentation_layers, conv_net)
+
+    # Build Classifier
+    classification_layers = compile_layers(conv_net_layers, classification_net)
+
+    output_layer = classification_layers
+
+    # Build model
+    model = Model(inputs=input_layer, outputs=output_layer)
+
+    return model
+
+# ------------------- Prepare Data ------------------- #
+
+# Configure Dataset for Performance
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+# ds_train = ds_train.cache()
+ds_train = ds_train.shuffle(1000).batch(batch_size).prefetch(buffer_size=AUTOTUNE)
+
+ds_test = ds_test.batch(batch_size).prefetch(buffer_size=AUTOTUNE)
+
+del x_train, y_train, x_test, y_test
+
+# ------------------- Prepare model ------------------- #\
+
+# Create model
+model = create_classifier()
+
+# # Summary
+# model.summary()
+# exit()
+
+# Compile model
+model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
+
+# Summary
+model.summary()
+
+# ------------------- Train model ------------------- #
+
+# Define train function
+def train_test_model(model):
+
+    # Callbacks
+    name_str = datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + '_' + model_name + '_' + str(0)
+
+    #   Save model callback
+    model_loc = 'models/' + name_str + '.h5'
+    checkpoint = ModelCheckpoint(model_loc, monitor='val_accuracy', 
+                                save_best_only=True, mode='max', verbose=1)
+    
+    print('Model saved at: ' + model_loc)
+
+    #   Tensorboard callback
+    log_dir = "logs/fit/" + name_str
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="logs/fit/" + name_str, histogram_freq=1)
+
+    print('tensorboard --logdir ' + log_dir)
+
+    # Train model
+    run_hist = model.fit(ds_train, validation_data=ds_test,
+                         epochs=epochs, batch_size=batch_size,
+                         callbacks=[checkpoint, tensorboard_callback])
+    
+    return run_hist
+
+# Run train and validation
+run_hist = train_test_model(model)
+
+# ------------------- Evaluate model ------------------- #
+
+# Evaluate model
+test_loss, test_acc = model.evaluate(ds_test, verbose=2)
+print(f'Test accuracy: {test_acc}')
